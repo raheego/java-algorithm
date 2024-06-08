5b825d7a1cdaaba92be4e2aafe336472aaefd887
raheego <54056684+raheego@users.noreply.github.com>
2024-06-08 15:18:06 +0900

    Update 개수 세기.java

--- 백준/Bronze/10807. 개수 세기/개수 세기.java
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -1,24 +1,36 @@
 import java.util.Scanner;
 
 public class Main {
-	public static void main(String[] args) throws Exception {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int[] arr = new int[n];
-		int count = 0;
-	    // 배열 입력
-	    for(int i = 0; i < n; i++) {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // 첫째 줄에 정수의 개수 N 입력
+        int n = sc.nextInt();
+
+        // 둘째 줄에 N개의 정수 입력
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
-	    // 비교하면서 출력
-	    int n2 = sc.nextInt();
-	    for(int i = 0; i < arr.length; i++) { 
-	    	if ( arr[i] == n2 ) { 
-	    		count ++;
-	    	}
-	    }
-	    
-	    //동일한 수 
-	    System.out.println(count);
-	}
+
+        // 셋째 줄에 찾으려고 하는 정수 v 입력
+        int v = sc.nextInt();
+
+        // v가 몇 개인지 세기 위한 변수 count
+        int count = 0;
+
+        // 배열을 순회하며 v의 개수 세기
+        for (int j = 0; j < arr.length; j++) {
+            if (arr[j] == v) {
+                count++;
+            }
+        }
+
+        // 결과 출력
+        System.out.println(count);
+
+        // Scanner 닫기
+        sc.close();
+    }
 }
