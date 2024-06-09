7d0d725872deae85e04f91e8b1b6133e9b6bec0e
raheego <54056684+raheego@users.noreply.github.com>
2024-06-09 16:59:43 +0900

    [level 0] Title: 두 수의 연산값 비교하기, Time: 1.24 ms, Memory: 74.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181938. 두 수의 연산값 비교하기/두 수의 연산값 비교하기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(int a, int b) {
+        int plusCase = Integer.valueOf((String.valueOf(a) + String.valueOf(b)));
+        int multi = 2 * a * b;
+        
+        return plusCase > multi ? plusCase : multi;
+    }
+}
