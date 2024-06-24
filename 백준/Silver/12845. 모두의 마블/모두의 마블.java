13d9e08cc12c90586b704bc4836f6a50c384d74d
raheego <54056684+raheego@users.noreply.github.com>
2024-06-24 23:16:22 +0900

    [Silver III] Title: 모두의 마블, Time: 132 ms, Memory: 14384 KB -BaekjoonHub

--- 백준/Silver/12845. 모두의 마블/모두의 마블.java
+++ 백준/Silver/12845. 모두의 마블/모두의 마블.java
@@ -7,23 +7,23 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        Integer[] cardLevel = new Integer[N];
+        int[] cardLevel = new int[N];
 
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        // 카드 레벨 입력 받기
         for(int i = 0; i < N; i++ ){
             cardLevel[i] = Integer.parseInt(st.nextToken());
         }
 
-        // 내림차순 정렬
-        Arrays.sort(cardLevel, Collections.reverseOrder());
+        // 가장 큰 카드 레벨을 찾아야 하므로 오름차순 정렬
+        Arrays.sort(cardLevel);
 
         int gold = 0; // 골드 초기화
+        int maxLevel = cardLevel[N - 1]; // 가장 큰 카드 레벨
 
-        // 가장 큰 레벨의 카드(cardLevel[0])와 다른 모든 카드를 합치면서 골드 계산
-        for (int i = 1; i < N; i++) {
-            gold += cardLevel[0] + cardLevel[i];
+        // 나머지 모든 카드를 최대 레벨의 카드와 합친다
+        for (int i = 0; i < N - 1; i++) {
+            gold += maxLevel + cardLevel[i];
         }
 
         System.out.println(gold);
